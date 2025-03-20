c03e25f2a73ab7d8e9077452e6b51c69a3d8eaa7
Sam Gabriel <smgbrl101010@gmail.com>
2025-03-20 22:03:30 +0800

    add minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        creamywhite: "#FFFDFA",
+        softblack: "#333333",
         brandBlue: "#1DA1F2",
         brandGray: "#657786",
+      },
+      fontFamily: {
+        inter: ["Inter", "Arial", "sans-serif"],
       },
     },
   },
